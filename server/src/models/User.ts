2591fe796c450bd8bace78b73482bde1ce535255
Oleh Matvii <oleh.m97@gmail.com>
2024-08-29 23:37:43 +0300

    feat: mongo schema

---
+++ server/src/models/User.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'Full name is required'],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters'],
+    },
+    avatarUrl: String,
+}, {
+    timestamps: true,
+});
+
+export default mongoose.model('User', UserSchema);
