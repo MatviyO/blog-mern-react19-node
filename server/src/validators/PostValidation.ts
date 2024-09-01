34abb775698521ed9e82161d992d83550fa46d49
Oleh Matvii <oleh.m97@gmail.com>
2024-09-01 21:51:49 +0300

    feat: jwt guard

--- server/src/validators/PostValidation.ts
+++ server/src/validators/PostValidation.ts
@@ -4,5 +4,5 @@ export const createPostValidation = [
     body('title', 'Title is required').isLength({min: 3}).isString(),
     body('description', 'Description is required').isLength({min: 3}).isString(),
     body('avatarUrl', 'Avatar url is optional and should be url').optional().isURL().isString(),
-    body('tags', 'Tags is required').isArray(),
+    body('tags', 'Tags is optional').optional().isArray(),
 ]
